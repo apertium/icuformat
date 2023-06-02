3e8b89f9f0a5ab02585443f00ec960bb316b1570
Ahmed Siam <aahs.coder@gmail.com>
2023-06-02 16:21:07 +0300

    test code

--- src/test.cpp
+++ src/test.cpp
@@ -4,15 +4,25 @@
 #include <unicode/utypes.h>
 #include "i18n.h"
 int main() {
-/*
+    /*
     UErrorCode status = U_ZERO_ERROR;
-    u_setDataDirectory(LOCALES_DIR);
-    void *data = udata_open("locales", "res", "locales", &status);
+    UDataMemory *locales_dat = NULL;
+    locales_dat = udata_open(LOCALES_DIR, "dat", "locales", &status);
     if (!U_SUCCESS(status)) {
         std::cerr << status << std::endl;
         exit(EXIT_FAILURE);
     }
-*/
+
+    // UDataInfo *pInfo = NULL;
+    // udata_getInfo(data, pInfo);
+    // std::cout << pInfo->size << std::endl;
+
+    udata_setAppData("locales.dat", udata_getMemory(locales_dat), &status);
+    if (!U_SUCCESS(status)) {
+        std::cerr << "setAppData" << std::endl;
+        std::cerr << status << std::endl;
+        exit(EXIT_FAILURE);
+    }*/
     I18n i18n {LOCALES_DIR};
 
     std::cout << i18n.format("test_message") << std::endl;
